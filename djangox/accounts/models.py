9698ea2be99b6353363d43d911c6b7d65edbad42
matejmecka <matej.plavevski+github@gmail.com>
2021-01-16 01:54:54 +0100

    some design stuff

--- djangox/accounts/models.py
+++ djangox/accounts/models.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
 from django.db import models
 
 class CustomUser(AbstractUser):
@@ -6,3 +7,13 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+class StellarAccount(models.Model):
+    """
+    Model representing the relationship between a user and a Stellar account
+    """
+    accountId =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    public_key = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.accountId
